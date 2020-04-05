70f5be8de4c0ff872a837aacf44fbbab610777b1
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-05 18:30:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+   def new
+    @post = Post.new
+  end
 
   def edit
     @post = Post.find(params[:id])
