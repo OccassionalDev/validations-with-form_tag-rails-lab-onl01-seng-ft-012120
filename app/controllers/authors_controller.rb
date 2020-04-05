2153a8eac9e8a20305ec6f6ecf234f72483204d6
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-05 18:33:26 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -24,6 +24,12 @@ class AuthorsController < ApplicationController
   
   def update
     @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end 
 
   private
